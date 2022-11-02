ce175e1d33c27820013d6da73fe5b5c8c92f7edc
Momo <76620405+immxmmi@users.noreply.github.com>
2022-11-03 00:55:20 +0100

    Modify the js code

--- scripts/game-service.js
+++ scripts/game-service.js
@@ -3,82 +3,85 @@
  * However, you are not allowed to change the signature of the exported functions and objects.
  */
 
+// eslint-disable-next-line camelcase
+
 const DELAY_MS = 1000;
 const playerStats = {
-  Markus: {
-    user: 'Markus',
-    win: 3,
-    lost: 6,
-  },
-  Michael: {
-    user: 'Michael',
-    win: 4,
-    lost: 5,
-  },
-  Lisa: {
-    user: 'Lisa',
-    win: 4,
-    lost: 5,
-  },
+    Markus: {
+        user: 'Markus',
+        win: 3,
+        lost: 6,
+    },
+    Michael: {
+        user: 'Michael',
+        win: 4,
+        lost: 5,
+    },
+    Lisa: {
+        user: 'Lisa',
+        win: 4,
+        lost: 5,
+    },
 };
 
 // TODO: Update this function to do the right thing
 function getRankingsFromPlayerStats() {
-  Object.keys(playerStats);
-  // magic happens
-  return [
-    {
-      rank: 1,
-      wins: 4,
-      players: ['Michael', 'Lisa'],
-    },
-    {
-      rank: 2,
-      wins: 3,
-      players: ['Markus'],
-    },
-  ];
+    Object.keys(playerStats);
+    // magic happens
+    return [
+        {
+            rank: 1,
+            wins: 4,
+            players: ['Michael', 'Lisa'],
+        },
+        {
+            rank: 2,
+            wins: 3,
+            players: ['Markus'],
+        },
+    ];
 }
 
 export const HANDS = ['Schere', 'Stein', 'Papier', 'Echse', 'Spock'];
 
+// CONNECTION
 let isConnectedState = false;
 
 export function setConnected(newIsConnected) {
-  isConnectedState = Boolean(newIsConnected);
+    isConnectedState = Boolean(newIsConnected);
 }
 
 export function isConnected() {
-  return isConnectedState;
+    return isConnectedState;
 }
 
 export function getRankings(rankingsCallbackHandlerFn) {
-  const rankingsArray = getRankingsFromPlayerStats();
-  setTimeout(() => rankingsCallbackHandlerFn(rankingsArray), DELAY_MS);
+    const rankingsArray = getRankingsFromPlayerStats();
+    setTimeout(() => rankingsCallbackHandlerFn(rankingsArray), DELAY_MS);
 }
 
 const evalLookup = {
-  scissors: {
-    scissors: 0,
-    stone: 1,
-    paper: -1,
-  },
+    scissors: {
+        scissors: 0,
+        stone: 1,
+        paper: -1,
+    },
 };
 
 function getGameEval(playerHand, systemHand) {
-  return evalLookup[playerHand][systemHand];
+    return evalLookup[playerHand][systemHand];
 }
 
 console.log('eval scissors-scissors: ', getGameEval('scissors', 'scissors'));
 
 export function evaluateHand(playerName, playerHand, gameRecordHandlerCallbackFn) {
-  // TODO: Replace calculation of didWin and update rankings while doing so.
-  // optional: in local-mode (isConnected == false) store rankings in the browser localStorage https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API
-  const systemHand = HANDS[Math.floor(Math.random() * 3)];
-  const gameEval = Math.floor(Math.random() * 3) - 1; // eval and hand do not match yet -> TODO
-  setTimeout(() => gameRecordHandlerCallbackFn({
-    playerHand,
-    systemHand,
-    gameEval,
-  }), DELAY_MS);
+    // TODO: Replace calculation of didWin and update rankings while doing so.
+    // optional: in local-mode (isConnected == false) store rankings in the browser localStorage https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API
+    const systemHand = HANDS[Math.floor(Math.random() * 3)];
+    const gameEval = Math.floor(Math.random() * 3) - 1; // eval and hand do not match yet -> TODO
+    setTimeout(() => gameRecordHandlerCallbackFn({
+        playerHand,
+        systemHand,
+        gameEval,
+    }), DELAY_MS);
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,16 @@
-import {HANDS, isConnected, getRankings, evaluateHand} from './game-service.js';
+import {HANDS, isConnected, getRankings, evaluateHand, setConnected} from './game-service.js';
+
+const playButton = document.querySelector('#play');
+const table = document.querySelector('#highscore');
+// eslint-disable-next-line camelcase
+const server_Button = document.querySelector('#server');
+const username = document.querySelector('#username');
+// eslint-disable-next-line camelcase
+const player_name = document.querySelector('#player_name');
+const back = document.querySelector('#back');
+const start = document.querySelector('#start');
+const game = document.querySelector('#game');
+
 // TODO: Create DOM references
 // TODO: How to keep track of App state?
 
@@ -8,28 +20,57 @@ import {HANDS, isConnected, getRankings, evaluateHand} from './game-service.js';
 
 // TODO: Replace the following demo code. It should not be included in the final solution
 
-console.log('isConnected:', isConnected());
+// RANKING //
+getRankings((rankings) => rankings.forEach((rankingEntry) => {
+    const row = table.insertRow();
+    const rank = row.insertCell(0);
+    const name = row.insertCell(1);
+    const score = row.insertCell(2);
+    rank.innerHTML = rankingEntry.rank;
+    score.innerHTML = rankingEntry.wins;
+    name.innerHTML = rankingEntry.players;
+}));
 
-getRankings((rankings) => rankings.forEach((rankingEntry) => console.log(
-  `Rank ${rankingEntry.rank} (${rankingEntry.wins} wins): ${rankingEntry.players}`,
-)));
+server_Button.onclick = function () {
+    if (isConnected()) {
+        server_Button.innerHTML = 'Offline';
+        setConnected(false);
+    } else {
+        server_Button.innerHTML = 'Online';
+        setConnected(true);
+    }
+
+    console.log('Connection: ', isConnected());
+};
+
+playButton.onclick = function () {
+    player_name.innerHTML = username.value;
+    start.style.display = 'none';
+    game.style.display = '';
+};
+
+back.onclick = function () {
+    player_name.innerHTML = username.value;
+    game.style.display = 'none';
+    start.style.display = '';
+};
 
 function pickHand() {
-  const handIndex = Math.floor(Math.random() * 3);
-  return HANDS[handIndex];
+    const handIndex = Math.floor(Math.random() * 3);
+    return HANDS[handIndex];
 }
 
 let count = 1;
 
 function printWinner(hand, didWin) {
-  console.log(count++, hand, didWin);
+    console.log(count++, hand, didWin);
 }
 
 for (let i = 1; i < 10; i++) {
-  const playerHand = pickHand();
-  evaluateHand('peter', playerHand,
-    ({
-       systemHand,
-       gameEval,
-     }) => printWinner(playerHand, systemHand, gameEval));
+    const playerHand = pickHand();
+    evaluateHand('peter', playerHand,
+        ({
+             systemHand,
+             gameEval,
+         }) => printWinner(playerHand, systemHand, gameEval));
 }
